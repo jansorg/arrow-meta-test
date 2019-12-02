a78e9e62b57eede8191d356d63c7299c55d9c6f1
jansorg <github@joachim-ansorg.de>
2019-12-02 14:34:40 +0100

    use new gradle plugin and Kotlin 1.3.60

--- src/main/kotlin/testArrow/Person.kt
+++ src/main/kotlin/testArrow/Person.kt
@@ -1,12 +1,12 @@
-////metadebug
-//package arrow
-//
-//data class Person(val name: String, val age: Int)
-//
-//fun main(args: Array<String>) {
-//    val steve = Person("Steve", age=12)
-//    val name = Person.name.get(steve)
-//    val age = Person.age.get(steve)
-//
-//    println("$name is $age years old")
-//}
+//metadebug
+package testArrow
+
+data class Person(val name: String, val age:  Int)
+
+fun main(args: Array<String>) {
+    val steve = Person("Steve", age=42)
+    val name = Person.name.get(steve)
+    val age = Person.age.get(steve)
+
+    println("$name is $age years old")
+}
--- src/main/kotlin/testArrow/higher.kt
+++ src/main/kotlin/testArrow/higher.kt
@@ -1,24 +1,8 @@
-//metadebug
+package testArrow
 
-package arrow
-
-@higherkind
-class Id2<out A>(val value: A)
+import arrow.higherkind
 
 @higherkind
-class Id3<out A, out B>(val value: A, val value2: B)
+class Id<out A>(val value: A)
 
-val intID =  Id2(1)
-val stringID: Id2Of<String> = Id2("foo")
-
-val id3 = Id3("foo bassr", 123)
-
-fun main(args: Array<String>) {
-    val x : Kind<String, String>
-
-    println(intID.fix().value)
-    println(stringID.fix().value)
-    println(id3)
-    println(id3.fix())
-    println(id3.fix().value2)
-}
+val x: IdOf<Int> = Id(42)
