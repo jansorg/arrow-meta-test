a7d5f5ed537cc708b48b8cff1f742180a6fa7626
jansorg <github@joachim-ansorg.de>
2020-04-01 15:03:49 +0200

    Make it a simple test

--- src/main/kotlin/testArrow/higher.kt
+++ src/main/kotlin/testArrow/higher.kt
@@ -6,3 +6,10 @@ import arrow.higherkind
 class Id<out A>(val value: A)
 
 val x: IdOf<Int> = Id(42)
+
+fun main() {
+    val x :IdOf<Int> = Id(12).fix()
+    val y :IdOf<String> = Id("hello").fix()
+    val z : IdOf<Float> = Id(123f).fix()
+    val w : IdOf<Int> = Id(123).fix()
+}
